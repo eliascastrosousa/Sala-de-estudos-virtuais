fdeb240d9451221a868683030f2e938026c553bb
renantinti <renantinti@gmail.com>
2023-03-18 21:53:32 -0300

    logica para usuarios criada - necessario corrigir autenticaão

--- salas-virtuais/chat/urls.py
+++ salas-virtuais/chat/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import lobby
+from .views import *
 
 urlpatterns = [
-    path('', lobby),
+    path('', landing, name='landing'),
+    path('login', login_page, name='login'),
+    path('register', register_page, name='register'),
+    path('lobby', lobby, name='lobby'),
+    path('rooms', rooms, name='rooms'),
+    path('chat', chat, name='chat'),
 ]
--- salas-virtuais/chat/views.py
+++ salas-virtuais/chat/views.py
@@ -1,5 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .decorators import unauthenticated_user
+
+from django.contrib.auth.models import User
 
 # Create your views here.
+def landing(request):
+    return render(request, 'landing.html')
+
+@unauthenticated_user
+def register_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+
+        if password1 == password2:
+            user = User(username=username, email=email, password=password1)
+            user.save()
+        else:
+            messages.error(request, 'As senhas não coincidem')
+
+        messages.success(request, 'Conta criada com sucesso para ' + user.username)
+        return redirect('login')
+        
+    return render(request, 'register.html')
+
+@unauthenticated_user
+def login_page(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('lobby')
+
+        else:
+            messages.info(request, 'E-mail ou senha incorretos')
+
+    return render(request, 'login.html')
+
+@login_required(login_url='login')
 def lobby(request):
     return render(request, 'lobby.html')
+
+@login_required(login_url='login')
+def rooms(request):
+    return render(request, 'rooms.html')
+
+@login_required(login_url='login')
+def chat(request):
+    return render(request, 'chat.html')
