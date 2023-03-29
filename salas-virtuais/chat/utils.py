a3d78ffe5c4c658291b6e66334bebee6d0e3ec94
renantinti <renantinti@gmail.com>
2023-03-29 12:41:22 -0300

    user levels

--- salas-virtuais/chat/utils.py
+++ salas-virtuais/chat/utils.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect
 
+ADMIN_PROVIDER = 'aluno.ifsp.edu.br'
+
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -9,3 +11,11 @@ def unauthenticated_user(view_func):
             return view_func(request, *args, **kwargs)
     
     return wrapper_func
+
+
+def check_if_superuser(email: str) -> bool:
+    splitted_email = email.split('@')
+    email_provider = splitted_email[1]
+    
+    return True if email_provider == ADMIN_PROVIDER else False
+    
